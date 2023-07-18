30989c46f167eca8afe63e31660afbf95e72fdcf
Pab10Suarez <retroguias47@gmail.com>
2023-07-17 22:40:18 -0500

    Movimiento pj

---
+++ Assets/Scripts/Pablo/Pablo_Controller.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Pablo_Controller : MonoBehaviour
+{
+//----movimiento----
+    Rigidbody2D rb;
+    Vector2 movementInput;
+    float movespeed=2f;
+    bool canMove=true;
+    bool isMoving=true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    void FixedUpdate()
+    {
+        if(canMove==true&&movementInput !=Vector2.zero){
+            rb.MovePosition(rb.position + movementInput * movespeed * Time.fixedDeltaTime);
+            isMoving=true;
+            Debug.Log(isMoving);
+        }
+        else{
+            isMoving=false;
+        }
+    }
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+        
+    }
+}
