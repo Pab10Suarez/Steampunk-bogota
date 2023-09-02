49a5993d446f693996cb59d08a0b58e12931046e
Camilo Alejandro <Camiloalejo2006@gmail.com>
2023-09-01 22:18:44 -0500

    Avatares puestos y misión mejorada

--- Assets/Scripts/Diálogos/Goal.cs
+++ Assets/Scripts/Diálogos/Goal.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    [SerializeField] private GameObject goal;
+    [SerializeField] private GameObject goal, dialogueChest;
     [SerializeField] private Dialogue dialogue;
+    private bool readyToCatch;
 
     public void StartMission()
     {
         goal.SetActive(true);
+        dialogueChest.SetActive(false);
+        readyToCatch = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (readyToCatch && Input.GetKeyDown(KeyCode.E))
         {
             goal.SetActive(false);
             dialogue.Carry();
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            readyToCatch = true;
+            dialogueChest.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            dialogueChest.SetActive(false);
+            readyToCatch = false;
+        }
+    }
 }
